bb688ccae38d9771151aeb40c467a6aa0ed0a3e5
Gereon Elvers <gereonelvers99@gmail.com>
2023-05-05 21:46:34 +0200

    Working deployment

--- Dockerfile
+++ Dockerfile
@@ -1,20 +1,59 @@
-# Using the official Node.js base image
-FROM node
+# Replace the version with the desired Node.js version
+FROM node:20
 
 # Set the working directory
-WORKDIR /usr/src/app
+WORKDIR /app
 
-# Copy the package.json and package-lock.json files
+# Install required libraries and packages for Puppeteer
+RUN apt-get update && apt-get install -y \
+      libnss3 \
+      libxss1 \
+      libgbm1 \
+      libasound2 \
+      libatk1.0-0 \
+      libatk-bridge2.0-0 \
+      libc6 \
+      libcairo2 \
+      libcups2 \
+      libdbus-1-3 \
+      libexpat1 \
+      libfontconfig1 \
+      libgcc1 \
+      libgconf-2-4 \
+      libgdk-pixbuf2.0-0 \
+      libegl1-mesa \
+      libgbm-dev \
+      libglib2.0-0 \
+      libgtk-3-0 \
+      libnspr4 \
+      libpango-1.0-0 \
+      libpangocairo-1.0-0 \
+      libx11-6 \
+      libx11-xcb1 \
+      libxcb1 \
+      libxcomposite1 \
+      libxcursor1 \
+      libxdamage1 \
+      libxext6 \
+      libxfixes3 \
+      libxi6 \
+      libxrandr2 \
+      libxrender1 \
+      libxslt1.1 \
+      libxt6 \
+      libxtst6 \
+      libsm6 \
+      libxshmfence1 \
+      libdrm2
+
+# Copy package.json and package-lock.json
 COPY package*.json ./
 
-# Install the required packages
+# Install dependencies
 RUN npm install
 
-# Copy the app.ts and other needed files
+# Copy source code
 COPY . .
 
-# Expose the PORT from the environment variable
-EXPOSE ${PORT}
-
-# Run the application
-CMD npx ts-node app.ts
+# Start the application
+CMD ["npm", "start"]
--- app.js
+++ app.js
@@ -1,45 +1,34 @@
-import { Configuration, OpenAIApi } from "openai";
-import {Message, MessageSearchOptions, RemoteAuth} from "whatsapp-web.js";
+const { Configuration, OpenAIApi } = require('openai');
+const { Message, MessageSearchOptions, RemoteAuth } = require('whatsapp-web.js');
 
 const qrcode = require('qrcode-terminal');
 
 const { Client } = require('whatsapp-web.js');
-const client = new Client();
-
-// const { MongoStore } = require('wwebjs-mongo');
-// const mongoose = require('mongoose');
-// mongoose.connect(process.env.MONGODB_URI).then(() => {
-//     const store = new MongoStore({ mongoose: mongoose });
-//     const client = new Client({
-//         authStrategy: new RemoteAuth({
-//             store: store,
-//             backupSyncIntervalMs: 300000
-//         })
-//     });
-//
-//     client.initialize();
-// });
-// client.on('remote_session_saved', () => {
-//     console.log("Saved session")
-// });
+const client = new Client({
+    puppeteer: {
+        args: [
+            '--no-sandbox',
+        ],
+    },
+});
 
 const configuration = new Configuration({
     apiKey: process.env.OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
 
-client.on('qr', (qr: string) => {
-    qrcode.generate(qr, {small: true});
+client.on('qr', function (qr) {
+    qrcode.generate(qr, { small: true });
 });
 
-client.on('ready', () => {
+client.on('ready', function () {
     console.log('Client is ready!');
 });
 
 client.initialize();
 
 const systemPrompt = `
-Du bist nun der Assistent von \"Gereon\", einem Studenten aus München, Deutschland.
+Du bist nun der Assistent von "Gereon", einem Studenten aus München, Deutschland.
 Du wirst eine Reihe von Nachrichten von einem Freund von ihm erhalten und sollst dabei helfen, passende Antworten vorzuschlagen, die Gereon dann übernehmen oder anpassen kann.
     Gereon ist 23 Jahre alt und studiert Wirtschaftsinformatik an der TU München (TUM).
     Sei freundlich und zuvorkommend, und verwende einen lockeren, humorvollen, informellen Schreibstil, der Gereons Art zu kommunizieren ähnelt.
@@ -47,21 +36,16 @@ Du wirst eine Reihe von Nachrichten von einem Freund von ihm erhalten und sollst
     Du antwortest immer aus der Sicht von Gereon und kommunizierst entsprechend niemals mit ihm.
     `
 
-client.on('message', async (message: Message) => {
+client.on('message', async function (message) {
     console.log("Received message:");
     console.log(message.body);
-    // history = (await message.getChat()).fetchMessages({limit: 10, fromMe: false})
-    // const messageHistory = history.map((msg) => ({
-    //     role: msg.fromMe ? 'assistant' : 'user',
-    //     content: msg.body
-    // }));
 
     if (message.body.includes('!gpt')) {
         const completion = await openai.createChatCompletion({
             model: "gpt-4",
             messages: [
-                {role: "system", content: systemPrompt},
-                {role: "user", content: message.body}
+                { role: "system", content: systemPrompt },
+                { role: "user", content: message.body }
             ],
         });
         if (completion && completion.data && completion.data.choices[0] && completion.data.choices[0].message) {
